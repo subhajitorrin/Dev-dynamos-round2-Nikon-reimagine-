3a639fe69eee57104a0823e505ec68599a5a8018
Abhradip Roy <abhra0312@gmail.com>
2024-07-24 18:18:15 +0530

    swiper caraousel on scroll

--- src/components/ExpendaingSwiper/Myswiper.jsx
+++ src/components/ExpendaingSwiper/Myswiper.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { Navigation, Pagination, A11y } from "swiper/modules";
+import React, { useEffect, useState ,useRef} from "react";
+import { Navigation, Pagination, A11y ,Mousewheel } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
 import "./Myswipper.css";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 const Myswipper = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   // const [abhra, setAbhra] = useState(0);
-
+  const SwiperIns = useRef();
   let abhra = 0
   useEffect(() => {
     const handleResize = () => {
@@ -22,6 +25,8 @@ const Myswipper = () => {
   }, []);
 
   useEffect(() => {
+    
+
     if (windowWidth <= 768) {
       let backslide = document.querySelectorAll(".back_slide");
       backslide.forEach((item) => {
@@ -36,17 +41,23 @@ const Myswipper = () => {
           console.log(e);
           console.log(abhra);
           if (abhra) {
-            slide.style.transform = "translateY(0)";
+            slide.style.transform = "translateY(0) scale(1)";
             backslide[i].style.scale = 0;
+            swiperInstance.update();
             abhra = 0
+            
           } else {
-            slide.style.transform = "translateY(-25px)";
+            slide.style.transform = "translateY(-60px) scale(0.65)";
             backslide[i].style.scale = 1;
+            swiperInstance.update();
             abhra = 1
           }
         });
       });
-    }
+    };
+    return () => {
+     timeline.scrollTrigger.kill(true);
+    };
   }, [windowWidth]);
 
   function handleSlide() {
@@ -60,16 +71,18 @@ const Myswipper = () => {
       item.style.scale= 0;
     });
   }
-
   return (
     <div className="SwipperMain">
-      {/* <div className="test">Latest News & Articles <FaArrowRightLong className="Arrow"/></div> */}
+ 
       <Swiper
+        ref={SwiperIns}
         id="mainswipper"
-        modules={[Navigation, Pagination, A11y]}
+        modules={[Navigation, Pagination, A11y , Mousewheel]}
         spaceBetween={0}
         mousewheel={{
-          invert:true,
+          enabled:true,
+          sensitivity: 1,
+          releaseOnEdges:true,
         }}
         slidesPerView={3}
         pagination={{ clickable: false }}
@@ -96,8 +109,7 @@ const Myswipper = () => {
             <div className="BackSlideProductInformation">
             <div className="NewsProduct"><p>New Delhi, May 23, 2024 </p>Nikon India Private Ltd. is pleased to announce that the total production of NIKKOR lenses for Nikon interchangeable lens cameras reached 120 million1 as of April, 2024.
               As one of the world’s few manufacturers that begins its manufacturing process from the <a href=""><u>See More</u></a></div>
-             
-            </div>
+          </div>
           </div>
         </SwiperSlide>
 
