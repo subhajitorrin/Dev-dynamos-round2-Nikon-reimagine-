01a4860a858cea5a4de3bb6cb8c9a2811bf46b69
Subhajit Ghosh <subhajitghosh343@gmail.com>
2024-07-25 22:46:26 +0530

    fix mobile loader

--- src/components/Loader/Loaderpage.jsx
+++ src/components/Loader/Loaderpage.jsx
@@ -7,6 +7,17 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import ButtonComponent from "../AccessoryComponent/spacebutton/ButtonComponent";
 function LoaderPage() {
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleresize() {
+      setWidth(window.innerWidth);
+    }
+    document.addEventListener("resize", handleresize);
+    return () => {
+      document.removeEventListener("resize", handleresize);
+    };
+  }, []);
+
   const [counter, setcounter] = useState(0);
   const [islaoding, setislaoding] = useState(true);
   // const [removeLoaderScreen, setremoveLoaderScreen] = useState(false);
@@ -45,50 +56,92 @@ function LoaderPage() {
     };
   };
 
-  return (
-    <>
-      {true && (
-        <div
-          id="mainloadercontainer"
-          className=" loadCon w-screen h-screen z-[9999999] text-white fixed flex flex-col items-center justify-center bg-[#010413] gap-4"
-        >
-          <div className="flex items-center justify-center gap-[1rem] relative top-[33px]">
-            <span className="golos LoaderNik">NIK</span>
-            <img className="LoaderImg" src={CameraSvg} alt=""></img>
-            <span className="golos LoaderOn">N</span>
-            <div className="space"></div>
-          </div>
-          <div className="h-[50px] flex flex-col gap-[10px] items-center w-full relative top-[5%] ">
-            {islaoding ? (
-              <>
-                <p>{counter}%</p>
-                <div className=" h-[7px] w-[60%] rounded-[5px]">
-                  <div
-                    className="h-full bg-white transition ease-linear transition-300"
-                    style={{ width: `${counter}%` }}
-                  ></div>
+  if (width > 768) {
+    return (
+      <>
+        {true && (
+          <div
+            id="mainloadercontainer"
+            className=" loadCon w-screen h-screen z-[9999999] text-white fixed flex flex-col items-center justify-center bg-[#010413] gap-4"
+          >
+            <div className="flex items-center justify-center gap-[1rem] relative top-[33px]">
+              <span className="golos LoaderNik">NIK</span>
+              <img className="LoaderImg" src={CameraSvg} alt=""></img>
+              <span className="golos LoaderOn">N</span>
+              <div className="space"></div>
+            </div>
+            <div className="h-[50px] flex flex-col gap-[10px] items-center w-full relative top-[5%] ">
+              {islaoding ? (
+                <>
+                  <p>{counter}%</p>
+                  <div className=" h-[7px] w-[60%] rounded-[5px]">
+                    <div
+                      className="h-full bg-white transition ease-linear transition-300"
+                      style={{ width: `${counter}%` }}
+                    ></div>
+                  </div>
+                </>
+              ) : (
+                <div style={{ opacity: islaoding ? "0" : "1" }} className="">
+                  <button
+                    className="textaniorrin px-[2rem] py-[10px] border border-white rounded-[10px] font-[700]"
+                    onClick={() => {
+                      gsap.to("#mainloadercontainer", {
+                        opacity: 0,
+                        pointerEvents: "none",
+                      });
+                    }}
+                  >
+                    Enter Experience
+                  </button>
                 </div>
-              </>
-            ) : (
-              <div style={{ opacity: islaoding ? "0" : "1" }} className="">
-                <button
-                  className="textaniorrin px-[2rem] py-[10px] border border-white rounded-[10px] font-[700]"
-                  onClick={() => {
-                    gsap.to("#mainloadercontainer", {
-                      opacity: 0,
-                      pointerEvents: "none",
-                    });
-                  }}
-                >
-                  Enter Experience
-                </button>
-              </div>
-            )}
+              )}
+            </div>
           </div>
-        </div>
-      )}
-    </>
-  );
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        {true && (
+          <div
+            id="mainloadercontainer"
+            className=" loadCon w-screen h-screen z-[999999999999] text-white fixed flex flex-col items-center justify-center bg-[#010413] gap-4"
+          >
+            <div className="NIK text-[6rem] font-bold">NIKON</div>
+            <div className="h-[50px] flex flex-col gap-[10px] items-center w-full relative top-[5%] ">
+              {islaoding ? (
+                <>
+                  <p>{counter}%</p>
+                  <div className=" h-[7px] w-[60%] rounded-[5px]">
+                    <div
+                      className="h-full bg-white transition ease-linear transition-300"
+                      style={{ width: `${counter}%` }}
+                    ></div>
+                  </div>
+                </>
+              ) : (
+                <div style={{ opacity: islaoding ? "0" : "1" }} className="">
+                  <button
+                    className="textaniorrin px-[2rem] py-[10px] border border-white rounded-[10px] font-[700]"
+                    onClick={() => {
+                      gsap.to("#mainloadercontainer", {
+                        opacity: 0,
+                        pointerEvents: "none",
+                      });
+                    }}
+                  >
+                    Enter Experience
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
 }
 
 export default LoaderPage;
